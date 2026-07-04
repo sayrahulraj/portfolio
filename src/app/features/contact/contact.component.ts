3e4451df5eb5a1391d288503855b92718866548f
Rahul Raj <rajdeveloper017@gmail.com>
2026-07-05 01:17:38 +0530

    first commit

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -32,6 +32,8 @@ export class ContactComponent {
   protected readonly submitted = signal(false);
   protected readonly success = signal(false);
 
+  protected readonly cityUrl = 'https://maps.google.com/?q=Hyderabad,India';
+
   protected readonly mapUrl: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.google.com/maps.app.goo.gl/gDBdbJXVeEaT5Jnm6');
 
   protected readonly form = this.fb.nonNullable.group({
